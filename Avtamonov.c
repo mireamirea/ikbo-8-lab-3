3d6bac138f3fffe958c4f06d2ae50b9ea6300fe6
avtamonov <avtamonovaralch@gmail.com>
2017-05-30 23:36:27 +0300

    Update Avtamonov.c

--- Avtamonov.c
+++ Avtamonov.c
@@ -3,51 +3,51 @@
  
 typedef struct tree
   {
-    int a;              // данные
-    struct tree *left;  // левый  сын
-    struct tree *right; // правый сын
+    int a;              
+    struct tree *left;  
+    struct tree *right; 
   } TREE;
  
 TREE *add_to_tree(TREE *root, int new_value)
 {
-   if (root==NULL)  // если нет сыновей - создаем новый элемент
+   if (root==NULL)  
      {
         root = (TREE*)malloc(sizeof(TREE));
         root->a = new_value;
         root->left = root->right = 0;
         return root;
      }
-   if (root->a < new_value)          // добавлем ветвь
+   if (root->a < new_value)          
      root->right = add_to_tree(root->right, new_value);
    else
      root->left  = add_to_tree(root->left,  new_value);
    return root;
 }
  
-void tree_to_array(TREE *root, int a[]) // процедура заполнения массива
+void tree_to_array(TREE *root, int a[]) 
   {
-    static max2=0;                      // счетчик элементов нового массива
-    if (root==NULL) return;             // условие окончания - нет сыновей
-    tree_to_array(root->left, a);       // обход левого поддерева
+    static max2=0;                      
+    if (root==NULL) return;             
+    tree_to_array(root->left, a);       
     a[max2++] = root->a;
-    tree_to_array(root->right, a);      // обход правого поддерева
+    tree_to_array(root->right, a);      
     free(root);
   }
  
-void sort_tree(int a[], int elem_total)        // собственно сортировка
+void sort_tree(int a[], int elem_total)        
 {
    TREE *root;
    int i;
  
    root = NULL;
-   for (i=0; i<elem_total; i++)        // проход массива и заполнение дерева
+   for (i=0; i<elem_total; i++)        
       root = add_to_tree(root, a[i]);
-   tree_to_array(root, a);             // заполнение массива
+   tree_to_array(root, a);            
 }
-  /* тестовая программа */
+ 
 void main() {
    int i;
-   /* Это будем сортировать */
+   
    int a[19]={ 0,7,8,3,52,14,16,18,15,13,42,30,35,26 ,456, 5 ,78,99,12345};
  
    sort_tree(a, 19);
